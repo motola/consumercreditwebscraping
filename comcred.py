852185988db9f6b111983cf30bcb788501be78d6
motola <olutolamoses@gmailcom>
2021-11-21 02:03:55 -0800

    getting the url

---
+++ comcred.py
@@ -0,0 +1,17 @@
+# importing necessary websraping tools to be used in the project
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import csv
+import re
+
+r = requests.get("https://www.consumeraffairs.com/travel/airbnb.html?page=1")
+
+soup = BeautifulSoup(r.text, 'html.parser')
+
+rating=[]
+authors=[]
+review=[]
+date=[]
+comment=[]
